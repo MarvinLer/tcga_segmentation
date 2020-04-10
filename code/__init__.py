dfa884539b92375c975455ace20eb95f759e2279
Marvin IGR <marvin.lerousseau@gmail.com>
2020-04-10 16:00:12 +0200

    bugfix double print in console/file for logger

--- code/__init__.py
+++ code/__init__.py
@@ -11,19 +11,20 @@ def get_logger(filename_handler, verbose=False):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
-    # file handler
-    fh = logging.FileHandler(filename_handler)
-    fh.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                                  datefmt='%d/%m/%Y %I:%M:%S %p')
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+    if not logger.handlers:
+        # file handler
+        fh = logging.FileHandler(filename_handler)
+        fh.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                      datefmt='%d/%m/%Y %I:%M:%S %p')
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
 
-    # console handler
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG if verbose else logging.INFO)
-    formatter = logging.Formatter('%(levelname)s     %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+        # console handler
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG if verbose else logging.INFO)
+        formatter = logging.Formatter('%(levelname)s     %(message)s')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
 
     return logger
