0286d09f9a177acfd2a5c6feb0d0f516e17aad2c
Marvin IGR <marvin.lerousseau@gmail.com>
2020-11-25 17:59:03 +0100

    moving logs to folder logs + now checks folder exists or create

--- code/__init__.py
+++ code/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'marvinler'
 
+import os
 import logging
-import sys
 
 # max number of processes for parallelism
 N_PROCESSES = 5
@@ -13,7 +13,10 @@ def get_logger(filename_handler, verbose=False):
 
     if not logger.handlers:
         # file handler
-        fh = logging.FileHandler(filename_handler)
+        filepath = os.path.join('logs', filename_handler)
+        if not os.path.exists(os.path.dirname(os.path.abspath(filepath))):
+            os.makedirs(os.path.dirname(os.path.abspath(filepath)))
+        fh = logging.FileHandler(filepath)
         fh.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                                       datefmt='%d/%m/%Y %I:%M:%S %p')
